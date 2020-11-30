63fb86d9873a0387f15f3b7ef380d3a57979291d
rin0207 <rintaku.7@gmail.com>
2020-11-30 23:40:00 +0900

    [Feature] rooting

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,20 @@ Rails.application.routes.draw do
   devise_for :admins
   devise_for :customers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+root "homes#top"
+
+namespace :public do
+	resources :customers, only: [:show, :update, :edit]
+	resources :cart_items, only: [:index, :create, :update, :destroy]
+	resources :items, only: [:index, :show]
+	resources :orders, only: [:new, :create, :index, :show]
+	resources :shipppings
+end
+
+namespace :admin do
+	resources :customers, only: [:index, :show, :edit, :update]
+	resources :genres, only: [:index, :create, :edit, :update]
+	resources :items
+	resources :orders
+end
 end
