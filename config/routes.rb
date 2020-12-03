dc0cc33f3d74a9a261a00ef77c55562c9df3fc9a
rin0207 <rintaku.7@gmail.com>
2020-12-03 23:54:03 +0900

    [Feature] admin/item

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :customers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root "homes#top"
+get "/about" => "homes#about"
 
 namespace :public do
 	resources :customers, only: [:show, :update, :edit]
