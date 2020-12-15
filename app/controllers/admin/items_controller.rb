bb97ec092ba58bc173e253e3d62c58ee2f37f7a7
rin0207 <rintaku.7@gmail.com>
2020-12-15 20:40:18 +0900

    [Feature] admin/item

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,2 +1,23 @@
 class Admin::ItemsController < ApplicationController
+	def index
+		@items = Item.all
+	end
+
+	def new
+		@item = Item.new
+	end
+
+	def crete
+		@item = Item.new(item_params)
+		@items = Item.all
+		if @item.save
+			redirect_to admin_items_path
+		else
+			redirect_back(fallback_location: admin_items_path)
+		end
+	end
+
+	def index
+		@items = Item.find(params[:id])
+	end
 end
