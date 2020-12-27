27a002a6d6c4f686242fb2c74e49507592a1eb03
rin0207 <rintaku.7@gmail.com>
2020-12-27 23:58:17 +0900

    [Feature] admin/genre

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,14 @@
 class Item < ApplicationRecord
+	validates :name, presence: true
+	validates :price, presence: true
+
+	belongs_to :genre, optional: true
+	has_many :cart_items, dependent: :destroy
+
+	attachment :image
+
+	enum sales_status:{
+		売り切れ: false,
+		販売中: true
+	}, _suffix: true
 end
