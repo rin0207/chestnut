513fc1f162a3140716e3100ccf9df7fe5981e233
rin0207 <rintaku.7@gmail.com>
2021-01-09 23:51:06 +0900

    [Feature] admin/customer

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,2 +1,42 @@
 class Admin::CustomersController < ApplicationController
+	def index
+		@items = Item.all
+	end
+
+	def new
+		@item = Item.new
+	end
+
+	def create
+		@item = Item.new(item_params)
+		@items = Item.all
+        if @item.save
+          redirect_to admin_items_path
+        else
+        redirect_back(fallback_location: admin_items_path)
+        end
+	end
+
+	def show
+		@item = Item.find(params[:id])
+	end
+
+	def edit
+		@item = Item.find(params[:id])
+	end
+
+	def update
+		@item = Item.find(params[:id])
+		@items = Item.all
+        if @item.update(item_params)
+          redirect_to admin_items_path(@item)
+        else
+        redirect_back(fallback_location: admin_items_path)
+        end
+	end
+
+	private
+    def item_params
+    params.require(:item).permit(:name, :price, :image, :introduction, :sales_status, :genre_id)
+    end
 end
