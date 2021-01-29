441a005ac9d4e3559faf2b198b54b1ce4b60bf0d
rin0207 <rintaku.7@gmail.com>
2021-01-29 15:35:43 +0900

    [Feature]admin/order

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,2 +1,22 @@
 class Admin::OrdersController < ApplicationController
+	def index
+		@orders = Order.page(params[:page]).reverse_order
+	end
+	def show
+	    @order = Order.find(params[:id])
+	    @order_items = @order.order_items.all
+	end
+	def update
+	    @order = Order.find(params[:id])
+        if @order.update(order_params)
+           redirect_to admin_order_path(@order)
+        else
+        render "show"
+        end
+	end
+
+	private
+    def order_params
+    params.require(:order).permit(:status)
+    end
 end
